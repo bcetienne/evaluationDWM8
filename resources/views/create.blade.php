51497c4506907649153d66e760ba3fd9274ed0e0
bcetienne <etienne1296pro@gmail.com>
2017-12-14 13:29:37 +0000

    Ajout du formulaire vide, mise en page simple

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,4 +2,24 @@
 @section('title', 'Ajout')
 @section('section')
     <h1 class="display-1">Ajout d'un produit au stock</h1>
+    <div class="container">
+        {!! Form::open(['url' => '/insert', 'class' => 'perso-form']) !!}
+            {{{ Form::label('nameLabel', 'Nom : ') }}} <br>
+            {{{ Form::text('nameField') }}}
+        <br>
+            {{{ Form::label('qtyLabel', 'Quantité : ') }}} <br>
+            {{{ Form::number('qtyField') }}}
+        <br>
+            {{{ Form::label('brandLabel', 'Marque : ') }}} <br>
+            {{{ Form::select('brandList') }}}
+        <br>
+            {{{ Form::label('genreLabel', 'Genres : ') }}} <br>
+            {{{ Form::select('genderList') }}}
+        <br>
+            {{{ Form::label('levelLabel', 'Niveau : ') }}} <br>
+            {{{ Form::select('levelList') }}}
+        <br>
+            {{{ Form::submit('Ajouter ce produit', ['class' => 'btn btn-outline-warning']) }}}
+        {!! Form::close() !!}
+    </div>
 @endsection
