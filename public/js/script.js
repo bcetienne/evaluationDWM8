2c577aa5be6eac90b8d895de0b64bad9d05ac13e
bcetienne <etienne1296pro@gmail.com>
2017-12-15 09:14:18 +0000

    Ajout du calcul du stock avec js

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,15 @@
 //TODO: Ajouter un changement de classe sur la barre de navigation pour que la classe <active> suive la page affichée
 //TODO: Ajouter le calcul automatique du stock
+
+/**
+ * Permet de calculer et d'ajouter le bon nombre au stock lors du clic sur le bouton
+ * Exemple si le calcul ne se fait pas : Il y a 3 articles, l'utilisateur pense en ajouter 4 alors, la valeur 4 sera
+ * récupéré et remplacera le 3 dans la base de données, donc au lieu de 7 articles il y en aura seulement 4 en stock.
+ */
+function calcStock() {
+    var stockActuel = Number(document.getElementById('qtyFieldExists').value);
+    var stockAjout = Number(document.getElementById('qtyFieldAdd').value);
+    var stockTotal = Number(stockActuel + stockAjout);
+
+    document.getElementById('qtyTotal').value = stockTotal;
+}
--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -12,11 +12,11 @@
         <br>
             {{--Affiche la valeur actuelle du stock dans la base de données et une vide--}}
             {{{ Form::label('qtyLabelExists', 'Quantité présente : ') }}} <br>
-            {{{ Form::number('qtyFieldExists', $product->quantity) }}}
+            {{{ Form::number('qtyFieldExists', $product->quantity, ['id' => 'qtyFieldExists']) }}}
         <br>
             {{{ Form::label('qtyLabelAdd', 'Quantité à ajouter : ') }}}<br>
-            {{{ Form::number('qtyFieldAdd') }}}
-            {{{ Form::number('qtyTotal') }}}
+            {{{ Form::number('qtyFieldAdd', 0, ['id' => 'qtyFieldAdd']) }}}
+            {{{ Form::hidden('qtyTotal', 0, ['id' => 'qtyTotal']) }}}
             {{--TODO: Récupérer la valeur des deux inputs, les additionner dans le total--}}
         <br>
             {{{ Form::label('brandLabel', 'Marque : ') }}} <br>
@@ -29,7 +29,7 @@
             {{--Je ne sais pas pourquoi, sans le placeholder, il ne se place pas automatiquement sur le niveau, il prend le premier de base (débutant)--}}
             {{{ Form::select('levelList', $levels, $product->level, ['placeholder' => $product->level->level]) }}}
         <br>
-            {{{ Form::submit('Modifier ce produit', ['class' => 'btn btn-outline-warning']) }}}
+            {{{ Form::submit('Modifier ce produit', ['class' => 'btn btn-outline-warning', 'onclick' => 'return calcStock()']) }}}
         {!! Form::close() !!}
     </div>
 @endsection
